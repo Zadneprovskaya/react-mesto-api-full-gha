c24ffc64ccc5cb7f08d741f040a6ff1fbac65048
Irina Zadneprovskaya <irinosik1182@gmail.com>
2023-09-24 16:00:47 +0300

    front_back30

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -230,7 +230,7 @@ function App() {
         if (data.token) {
           setEmail(email);
           handleLoggedIn();
-          localStorage.setItem('token', data.token);
+          //localStorage.setItem('token', data.token);
           navigate('../', { replace: true });
         }
       })
