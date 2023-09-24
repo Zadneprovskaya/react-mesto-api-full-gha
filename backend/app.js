e54d8a93b62a027eeaec9f0357f6f634b8b434b6
Irina Zadneprovskaya <irinosik1182@gmail.com>
2023-09-24 14:19:07 +0300

    front_back24

--- backend/app.js
+++ backend/app.js
@@ -28,11 +28,11 @@ app.use(helmet());
 app.use(express.json());
 app.use(requestLogger);
 
-/* app.get('/crash-test', () => {
+app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
-});  */
+}); 
 
 app.post('/signin', validateLogin, login);
 app.post('/signup', validateCreateUser, createUser);
